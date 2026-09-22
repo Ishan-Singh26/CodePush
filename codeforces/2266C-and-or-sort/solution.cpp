b11d09472d4273e3f1763437e83e99548b726101
Ishan-Singh26 <ishan260306@gmail.com>
2026-09-22 17:18:28 +0530

    CF 2266C: AND, OR, Sort!: add solution

---
+++ codeforces/2266C-and-or-sort/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int c0 = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '0') {
+                c0++;
+            }
+        }
+
+        if(s[0] == '1'){
+            cout<<c0<<'\n';
+            continue;
+        }
+
+        int ans = n;
+        int c1 = 0;
+
+        for(int i = 0; i <= n; i++) {
+            ans = min(ans, c1 + c0);
+
+            if(i < n) {
+                if(s[i] == '1') {
+                    c1++;
+                }
+                else {
+                    c0--;
+                }
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
